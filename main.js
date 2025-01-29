1f82d313a9dc45fee6d199051a13c42bd016f77d
yllcakiqi1 <you@example.com>
2025-01-29 16:18:54 +0100

    kemi bere funksion ne js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function showMessage() {
+
+    alert("This is message");
+}
+showMessage();
+function sum(num1,num2 ){
+return num1+num2;
+}
+console.log(sum (5,3));
+ 
+function toCelcius(f){
+  return(5/9)*(f-32);
+ }
+console.log("100 fahrenheit eshte e barabarte"+toCelcius(100)+"celcius");
